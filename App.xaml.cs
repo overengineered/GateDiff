5ce330fefa22b403c21a5736689ff84bc8758462
juozas.k <juozas@delfi.lt>
2014-01-12 23:02:59 +0200

    moved argument parsing to app class

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Data;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 using System.Windows;
 
 namespace GateDiff
@@ -13,5 +13,78 @@ namespace GateDiff
     /// </summary>
     public partial class App : Application
     {
+        public Tuple<FileInfo, FileInfo> DiffData
+        {
+            get;
+            set;
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var left = e.Args.Length >= 2 ? new FileInfo(e.Args[0]) : null;
+            var right = e.Args.Length >= 2 ? new FileInfo(e.Args[1]) : null;
+
+            String diffProgram = null;
+            if (left != null && right != null)
+            {
+                foreach (var key in ConfigurationManager.AppSettings.AllKeys)
+                {
+                    var extensions = key.Split(' ').Where(x => !String.IsNullOrWhiteSpace(x));
+                    var foundDiffer = extensions.Contains(left.Extension)
+                        || extensions.Contains(right.Extension)
+                        || extensions.Contains(".*");
+                    if (foundDiffer)
+                    {
+                        diffProgram = ConfigurationManager.AppSettings[key];
+                        diffProgram = String.IsNullOrWhiteSpace(diffProgram) ? null : diffProgram;
+                        break;
+                    }
+                }
+            }
+
+            if (diffProgram != null)
+            {
+                using (DiffFile leftFile = new DiffFile(left), rightFile = new DiffFile(right))
+                {
+                    ProcessStartInfo command = new ProcessStartInfo(diffProgram, BuildCommand(leftFile.Path, rightFile.Path));
+                    command.UseShellExecute = false;
+                    command.RedirectStandardOutput = true;
+                    Process app = Process.Start(command);
+
+                    app.StandardOutput.ReadToEnd();
+                    app.WaitForExit();
+
+                    Shutdown();
+                    return;
+                }
+            }
+
+            StartupUri = new System.Uri(@"MainWindow.xaml", System.UriKind.Relative);
+            DiffData = Tuple.Create(left, right);
+        }
+
+        private static string BuildCommand(params string[] args)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (string a in args)
+            {
+                if (string.IsNullOrEmpty(a))
+                    continue;
+
+                if (builder.Length > 0)
+                    builder.Append(@" ");
+                builder.Append(ArgumentEscape(a));
+            }
+            return builder.ToString();
+        }
+
+        private static string ArgumentEscape(string arg)
+        {
+            if (arg.Contains(@" "))
+                return @"""" + arg + @"""";
+            return arg;
+        }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,22 +1,7 @@
 ﻿using GateDiff.Properties;
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Interop;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GateDiff
 {
@@ -29,70 +14,8 @@ namespace GateDiff
         {
             InitializeComponent();
 
-            string[] args = Environment.GetCommandLineArgs();
-
-            var left = args.Length >= 3 ? new FileInfo(args[1]) : null;
-            var right = args.Length >= 3 ? new FileInfo(args[2]) : null;
-
-            String diffProgram = null;
-            if (left != null && right != null)
-            {
-                foreach (var key in ConfigurationManager.AppSettings.AllKeys)
-                {
-                    var extensions = key.Split(' ').Where(x => !String.IsNullOrWhiteSpace(x));
-                    var foundDiffer = extensions.Contains(left.Extension)
-                        || extensions.Contains(right.Extension)
-                        || extensions.Contains(".*");
-                    if (foundDiffer)
-                    {
-                        diffProgram = ConfigurationManager.AppSettings[key];
-                        diffProgram = String.IsNullOrWhiteSpace(diffProgram) ? null : diffProgram;
-                        break;
-                    }
-                }
-            }
-
-            if (diffProgram != null)
-            {
-                using (DiffFile leftFile = new DiffFile(left), rightFile = new DiffFile(right))
-                {
-                    ProcessStartInfo command = new ProcessStartInfo(diffProgram, BuildCommand(leftFile.Path, rightFile.Path));
-                    command.UseShellExecute = false;
-                    command.RedirectStandardOutput = true;
-                    Process app = Process.Start(command);
-
-                    app.StandardOutput.ReadToEnd();
-                    app.WaitForExit();
-
-                    Application.Current.Shutdown();
-                    return;
-                }
-            }
-
-            var data = Tuple.Create(left, right);
-            layoutRoot.DataContext = data;
-        }
-
-        private static string BuildCommand(params string[] args)
-        {
-            StringBuilder builder = new StringBuilder();
-            foreach (string a in args)
-            {
-                if (string.IsNullOrEmpty(a))
-                    continue;
-
-                if (builder.Length > 0)
-                    builder.Append(" ");
-                builder.Append(ArgumentEscape(a));
-            }
-            return builder.ToString();
-        }
-
-        private static string ArgumentEscape(string arg)
-        {
-            if (arg.Contains(@" "))
-                return @"""" + arg + @"""";
-            return arg;
+            var app = (App) App.Current;
+            layoutRoot.DataContext = app.DiffData;
         }
 
         protected override void OnSourceInitialized(EventArgs e)
