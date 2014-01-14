04346b2aad3df4e7dfe125e32b6ffc68dee850f7
juozas.k <juozas@delfi.lt>
2014-01-14 09:10:34 +0200

    added basic windows shell .NET extension

---
+++ Extension/GateContextMenu.cs
@@ -0,0 +1,75 @@
+﻿using SharpShell.Attributes;
+using SharpShell.SharpContextMenu;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace GateShell
+{
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.AllFiles)]
+    [SuppressMessage("Microsoft.Interoperability", "CA1405:ComVisibleTypeBaseTypesShouldBeComVisible")]
+    public class GateContextMenu : SharpContextMenu
+    {
+        private static readonly string MRU_KEY = "mru";
+        private Lazy<State> m_registry = new Lazy<State>(() => new State(8));
+
+        protected override bool CanShowMenu()
+        {
+            try
+            {
+                List<string> paths = SelectedItemPaths.ToList();
+                if (paths.Count == 1)
+                {
+                    return File.Exists(paths[0]);
+                }
+
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        protected override ContextMenuStrip CreateMenu()
+        {
+            ContextMenuStrip menu = new ContextMenuStrip();
+
+            var savedPaths = m_registry.Value.GetMruItems(MRU_KEY).ToList();
+            String mostRecentPath = savedPaths.FirstOrDefault();
+
+            if (mostRecentPath == null)
+            {
+                string menuLabel = "Remember";
+                ToolStripMenuItem item = new ToolStripMenuItem { Text = menuLabel };
+                item.Click += this.OnRemember;
+                menu.Items.Add(item);
+            }
+            else
+            {
+                string menuLabel = String.Format("Compare to {0}", System.IO.Path.GetFileName(mostRecentPath));
+                ToolStripMenuItem item = new ToolStripMenuItem { Text = menuLabel };
+                item.Click += this.OnCompare;
+                menu.Items.Add(item);
+            }
+
+            return menu;
+        }
+
+        private void OnRemember(object sender, EventArgs eventArgs)
+        {
+            var path = this.SelectedItemPaths.Single();
+            m_registry.Value.InsertMruItem(MRU_KEY, path);
+        }
+
+        private void OnCompare(object sender, EventArgs eventArgs)
+        {
+            MessageBox.Show("Not implemented", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
---
+++ Extension/State.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GateShell
+{
+    public sealed class State
+    {
+        private readonly string m_root;
+        private readonly int m_mruLimit;
+
+        public State(int mruLimit)
+        {
+            m_root = "SOFTWARE\\" + "overengineered" + "\\" + "GateShell";
+            m_mruLimit = mruLimit;
+        }
+
+        public void InsertMruItem(string listName, string value)
+        {
+            var lastSaved = RetrieveItemList(listName)
+                .Where(item => !String.Equals(item, value, StringComparison.InvariantCultureIgnoreCase))
+                .Take(m_mruLimit - 1)
+                .ToList();
+
+            SaveItemList(listName, Enumerable.Concat(Enumerable.Repeat(value, 1), lastSaved));
+        }
+
+        public IEnumerable<string> GetMruItems(string listName)
+        {
+            return RetrieveItemList(listName).Take(m_mruLimit);
+        }
+
+        public IEnumerable<string> RetrieveItemList(String name)
+        {
+            using (RegistryKey mainKey = Registry.CurrentUser.OpenSubKey(m_root, false))
+            {
+                if (mainKey == null)
+                    yield break;
+
+                using (RegistryKey listKey = mainKey.OpenSubKey(name))
+                {
+                    if (listKey == null)
+                        yield break;
+
+                    int index = 0;
+                    while (true)
+                    {
+                        var key = index.ToString();
+                        var value = (string) listKey.GetValue(key);
+                        if (String.IsNullOrEmpty(value))
+                            yield break;
+
+                        yield return value;
+                        index++;
+                    }
+                }
+            }
+        }
+
+        public void SaveItemList(String name, IEnumerable<string> values)
+        {
+            using (RegistryKey mainKey = Registry.CurrentUser.CreateSubKey(m_root))
+            {
+                if (mainKey == null)
+                    return;
+
+                using (RegistryKey listKey = mainKey.CreateSubKey(name))
+                {
+                    if (listKey == null)
+                        return;
+
+                    int index = 0;
+                    try
+                    {
+                        foreach (var value in values)
+                        {
+                            var key = index.ToString();
+                            listKey.SetValue(key, value);
+                            index++;
+                        }
+                    }
+                    finally
+                    {
+                        bool throwOnMissingValue = false;
+                        listKey.DeleteValue(index.ToString(), throwOnMissingValue);
+                    }
+                }
+            }
+        }
+    }
+}
