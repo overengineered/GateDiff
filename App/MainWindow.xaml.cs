738c1c24d8c138184bb1315ce70937177791ba73
juozas.k <juozas@delfi.lt>
2014-01-20 22:36:55 +0200

    added button to swap file comparison

--- App/MainWindow.xaml.cs
+++ App/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using GateDiff.Properties;
 using System;
+using System.IO;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Interop;
 
 namespace GateDiff
@@ -18,6 +20,16 @@ namespace GateDiff
             layoutRoot.DataContext = app.DiffData;
         }
 
+        private void ToolBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            ToolBar toolBar = sender as ToolBar;
+            var overflowGrid = toolBar.Template.FindName("OverflowGrid", toolBar) as FrameworkElement;
+            if (overflowGrid != null)
+            {
+                overflowGrid.Visibility = Visibility.Collapsed;
+            }
+        }
+
         protected override void OnSourceInitialized(EventArgs e)
         {
             base.OnSourceInitialized(e);
@@ -28,6 +40,12 @@ namespace GateDiff
         {
             Settings.Default.MainWindowPlacement = WindowPlacement.GetPlacement(new WindowInteropHelper(this).Handle);
             Settings.Default.Save();
+        }
+
+        private void Swap_Click(object sender, RoutedEventArgs e)
+        {
+            var data = (Tuple<FileInfo, FileInfo>)layoutRoot.DataContext;
+            layoutRoot.DataContext = new Tuple<FileInfo, FileInfo>(data.Item2, data.Item1);
         }
     }
 }
